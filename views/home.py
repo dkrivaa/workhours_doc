e063b84231852f51fbe60148468dc85b9cca554b
dkrivaa <dannykrivaa@hotmail.com>
2024-11-02 20:20:39 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -10,9 +10,9 @@ client = st.session_state['client']
 
 # Get list of sheets
 sheet_list = google_sheet_list()
-
+# Choose one of sheets
 sheet_name = st.selectbox('Choose sheet', options=sheet_list, index=None)
-st.write(sheet_name)
+# Getting the data from relevant sheet that hasn't been reported
 if sheet_name is not None:
     df = read_sheet(sheet_name)
     st.dataframe(df)
