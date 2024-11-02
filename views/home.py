3a45ae2b6b21c914619f3e9c700404e2d7877201
dkrivaa <dannykrivaa@hotmail.com>
2024-11-02 19:04:21 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -7,7 +7,9 @@ from google_funcs import google_client, google_sheet_list
 google_client()
 # Getting client
 client = st.session_state['client']
-# Get list of sheets
-sheet_list = google_sheet_list()
 
-st.selectbox('Choose sheet', options=sheet_list, index=None)
+st.write('test')
+# # Get list of sheets
+# sheet_list = google_sheet_list()
+#
+# st.selectbox('Choose sheet', options=sheet_list, index=None)
