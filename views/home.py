e9d6fbc6ef095c9857fdd21efaf928128c2c76e7
dkrivaa <dannykrivaa@hotmail.com>
2024-11-02 18:56:10 +0200

    initial commit

--- views/home.py
+++ views/home.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import os
+
 from google_funcs import google_client, google_sheet_list
 
-encoded_credentials = os.getenv("GOOGLE_CREDENTIALS_BASE64")
-st.write(encoded_credentials)
 
-# # Defining Google client and saving to session state
-# google_client()
-# # Getting client
-# client = st.session_state['client']
-# # Get list of sheets
-# sheet_list = google_sheet_list()
-#
-# st.selectbox('Choose sheet', options=sheet_list, index=None)
+# Defining Google client and saving to session state
+google_client()
+# Getting client
+client = st.session_state['client']
+# Get list of sheets
+sheet_list = google_sheet_list()
+
+st.selectbox('Choose sheet', options=sheet_list, index=None)
