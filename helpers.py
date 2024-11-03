36fd61e2f3d6c40ee5e8c496e9f2fbaab9d4039e
dkrivaa <dannykrivaa@hotmail.com>
2024-11-03 21:54:52 +0200

    initial commit

--- helpers.py
+++ helpers.py
@@ -5,7 +5,8 @@ import streamlit as st
 def number_docx():
     docx_option = st.radio('Choose an option',
                            options=['One docx for all unreported hours',
-                                    'One docx for each month'])
+                                    'One docx for each month'],
+                           index=None)
     if docx_option == 'One docx for all unreported hours':
         st.session_state['docx_option'] = 1
     elif docx_option == 'One docx for each month':
