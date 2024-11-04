247b2cbcf162a45b973b9fbc42a6fd5281907637
dkrivaa <dannykrivaa@hotmail.com>
2024-11-04 07:32:26 +0200

    initial commit

--- helpers.py
+++ helpers.py
@@ -11,7 +11,7 @@ def number_docx():
     st.write(docx_option)
     if docx_option == 'One docx for all unreported hours':
         st.session_state['docx_option'] = 1
-    elif docx_option == 'One docx for each month':
+    elif docx_option == 'A docx for each month containing unreported hours':
         st.session_state['docx_option'] = 2
     st.write('---')
     submit = st.button('Submit')
