62d78f54ec20c55a0391a81cdf824203fcf94993
dkrivaa <dannykrivaa@hotmail.com>
2024-11-04 09:24:44 +0200

    initial commit

--- docx_funcs.py
+++ docx_funcs.py
@@ -23,7 +23,7 @@ def one_docx(df):
 
     # Add date to doc
     add_date = document.add_paragraph(today)
-    add_date.space_after = Pt(200)
+    add_date.space_after = Pt(2000)
 
     # Add subject
     add_subject = document.add_paragraph()
