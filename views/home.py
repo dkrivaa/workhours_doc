f3bfedd31ce47d179324ecb3b1df3c80ad15291c
dkrivaa <dannykrivaa@hotmail.com>
2025-07-31 07:42:35 +0300

    initial commit

--- views/home.py
+++ views/home.py
@@ -26,6 +26,8 @@ if sheet_name is not None:
         st.subheader('Workhours to report:')
         df = read_sheet(sheet_name)
 
+        df.write(df)
+
         # If there are hours to report
         if len(df) > 0:
             st.dataframe(df)
